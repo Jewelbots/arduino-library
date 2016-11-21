d21a220f273fdca73592beb4b0270f7b26420bf5
Sara Chipps <sarajchipps@gmail.com>
2016-11-21 13:31:35 -0500

    changing function

--- jewelbots_nRF51822_board_v100/cores/JWB_nRF51822/LED.cpp
+++ jewelbots_nRF51822_board_v100/cores/JWB_nRF51822/LED.cpp
@@ -20,7 +20,6 @@
 
 extern "C"{
 
-uint8_t color_values[2];
 
 
 
@@ -35,7 +34,7 @@ uint8_t color_values[2];
   void LED::on(uint8_t number, char *color, uint8_t length)  {
     enable_led();
     clear_led();
-    this->color_lookup(color);
+    color_values[2] = color_lookup(color);
     led_cmd_t options[4] = {number, color_values[0], color_values[1], color_values[2], 1};
     set_led_state_handler(options);
     nrf_delay_us(length);
@@ -43,50 +42,6 @@ uint8_t color_values[2];
   }
 
 
-  void LED::color_lookup(char *color)
-  {
-    if(color == "red") {
-      color_values[0] = 0x3F;
-      color_values[1] = 0x00;
-      color_values[2] = 0x00;
-    }
-    else if (color == "orange"){
-      color_values[0] = 0x3F;
-      color_values[1] = 0x1C;
-      color_values[2] = 0x00;
 
-    }
-    else if (color == "yellow"){
-      color_values[0] = 0x3F;
-      color_values[1] = 0x3F;
-      color_values[2] = 0x00;
-
-    }
-    else if (color == "green"){
-      color_values[0] = 0x00;
-      color_values[1] = 0x3F;
-      color_values[2] = 0x00;
-    }
-    else if (color == "blue"){
-      color_values[0] = 0x00;
-      color_values[1] = 0x00;
-      color_values[2] = 0x3F;
-    }
-    else if (color == "violet"){
-      color_values[0] = 0x1F;
-      color_values[1] = 0x00;
-      color_values[2] = 0x3F;
-     }
-    else if (color == "pink"){
-      color_values[0] = 0x3F;
-      color_values[1] = 0x1A;
-      color_values[2] = 0x3A;
-      }
-    else if (color == "magenta"){
-      color_values[0] = 0x3F;
-      color_values[1] = 0x0C;
-      color_values[2] = 0x3F;
-      }
-  }
 
 } // extern "C"
