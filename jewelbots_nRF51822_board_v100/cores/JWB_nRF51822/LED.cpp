43fc7bdefc6f7c424dcd0408803897936dd73cee
Rachel J Morris <RachelJMorris@gmail.com>
2016-11-25 23:18:06 -0600

    Added functions to LED to turn on, off, and flash

--- jewelbots_nRF51822_board_v100/cores/JWB_nRF51822/LED.cpp
+++ jewelbots_nRF51822_board_v100/cores/JWB_nRF51822/LED.cpp
@@ -24,23 +24,44 @@ extern "C"{
 
 
   LED::LED()
-  {}
+  {
+    enable_led();
+    clear_led();
+  }
 
   LED::~LED()
   {}
 
-
-
   void LED::on(uint8_t number, char *color, uint8_t length)  {
-    enable_led();
-    clear_led();
     uint8_t *color_values = color_lookup(color);
     led_cmd_t options[4] = {number, color_values[0], color_values[1], color_values[2], 1};
     set_led_state_handler(options);
     nrf_delay_us(length);
     clear_led();
   }
+  
+  void LED::turnOn(LED_Pos led, ColorLabel color)
+  {
+      setLight(uint8_t(led), COLORS[ color ].r, COLORS[ color ].g, COLORS[ color ].b);
+  }
 
+  void LED::turnOff(LED_Pos led)
+  {
+      setLight(uint8_t(led), COLORS[ OFF ].r, COLORS[ OFF ].g, COLORS[ OFF ].b);
+  }
+  
+  void LED::flash(LED_Pos led, ColorLabel color, uint8_t microseconds)
+  {
+      turnOn(led, color);
+      nrf_delay_us(microseconds);
+      turnOff(led);
+  }
+  
+  void LED::setLight(uint8_t number, uint8_t r, uint8_t g, uint8_t b)
+  {
+    led_cmd_t options[4] = {number, r, g, b, 1};
+    set_led_state_handler(options);
+  }
 
 
 
