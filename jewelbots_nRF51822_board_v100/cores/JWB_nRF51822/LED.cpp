63a4b5faaca2eb05fbf1e477ffd90d332122bafe
Sara Chipps <sarajchipps@gmail.com>
2016-11-26 00:32:17 -0500

    now I'm just playing

--- jewelbots_nRF51822_board_v100/cores/JWB_nRF51822/LED.cpp
+++ jewelbots_nRF51822_board_v100/cores/JWB_nRF51822/LED.cpp
@@ -34,7 +34,7 @@ extern "C"{
   void LED::on(uint8_t number, char *color, uint8_t length)  {
     enable_led();
     clear_led();
-    uint8_t color_values[2] = color_lookup(color);
+    uint8_t *color_values = color_lookup(&color);
     led_cmd_t options[4] = {number, color_values[0], color_values[1], color_values[2], 1};
     set_led_state_handler(options);
     nrf_delay_us(length);
