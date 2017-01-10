c8f7e867f86f43df4eb5df6e0ac0981d8bf45782
Scott Davison <scott@jewelbots.com>
2017-01-10 13:56:44 -0500

    update to v1.0.1.  add runtime measurement

--- jewelbots_nRF51822_board_v101/cores/JWB_nRF51822/Timer.cpp
+++ jewelbots_nRF51822_board_v101/cores/JWB_nRF51822/Timer.cpp
@@ -18,7 +18,9 @@ extern "C"{
   {}
 
   APP_TIMER_DEF(pause_timer_id);
+  APP_TIMER_DEF(runtime_timer_id);
   static bool timer_flag = false;
+  static uint32_t start_ticks = 0;
 
   static void pause_timeout_handler(void *p_context) {
   	uint32_t err_code;
@@ -28,15 +30,28 @@ extern "C"{
     timer_flag = false;
   }
 
+  static void runtime_timeout_handler(void *p_context) { UNUSED_PARAMETER(p_context); }
+
   static uint32_t convert_ms_ticks(uint32_t milliseconds){
     uint32_t num_ticks;
     return num_ticks = ((uint32_t)ROUNDED_DIV((milliseconds) * (uint64_t)APP_TIMER_CLOCK_FREQ, ((APP_TIMER_PRESCALER) + 1) * 1000));
   }
 
+  static uint32_t convert_ticks_ms(uint32_t num_ticks){
+     uint32_t milliseconds;
+     return milliseconds = ((uint32_t)ROUNDED_DIV((num_ticks) * (((APP_TIMER_PRESCALER) + 1) * 1000), (uint64_t)APP_TIMER_CLOCK_FREQ));
+   }
+
   void arduino_timer_init(void) {
     uint32_t err_code;
     err_code = app_timer_create(&pause_timer_id, APP_TIMER_MODE_SINGLE_SHOT, pause_timeout_handler);
     APP_ERROR_CHECK(err_code);
+    err_code = app_timer_create(&runtime_timer_id, APP_TIMER_MODE_REPEATED, runtime_timeout_handler);
+    APP_ERROR_CHECK(err_code);
+
+    err_code = app_timer_start(runtime_timer_id, 480 * 32768, NULL);
+    APP_ERROR_CHECK(err_code);
+    app_timer_cnt_get(&start_ticks);
   }
 
   void Timer::pause(uint32_t milliseconds)
@@ -56,5 +71,12 @@ extern "C"{
     }
   }
 
+  uint32_t Timer::runtime_ms(void) {
+      uint32_t now_ticks;
+      uint32_t elapsed_ticks;
+      app_timer_cnt_get(&now_ticks);
+      app_timer_cnt_diff_compute(now_ticks, start_ticks, &elapsed_ticks);
 
+      return convert_ticks_ms(elapsed_ticks);
+    }
 }
